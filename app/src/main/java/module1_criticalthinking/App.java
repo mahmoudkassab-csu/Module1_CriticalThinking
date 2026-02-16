fbb0ce7cfb8bf41c83e4dc0a90c76e00b34c59b5
Mahmoud Kassab <mahmoud.kassab@csuglobal.eduyou@example.com>
2026-02-15 20:09:39 -0500

    adding code

--- app/src/main/java/module1_criticalthinking/App.java
+++ app/src/main/java/module1_criticalthinking/App.java
@@ -14,16 +14,18 @@
         e. Zip code: String
     3. Assign appropriate values to each variable
         a. First name: "John"
-        b. Last name: "Doe"
-        c. Street address: "123 Main St"
-        d. City: "Anytown"
-        e. Zip code: "12345"
-    3. Print the first name, last name, street address, city, and zip code to the console on a separate line
+        b. Last name: "Battle"
+        c. Street address: "2990 Cook Hill Road"
+        d. City: "Stamford"
+        e. State: "CT"
+        f. Zip code: "06903"
+    3. Print the first name, last name, street address, city, state, and zip code to the console on a separate line
         a. print "First Name: " + first name
         b. print "Last Name: " + last name
         c. print "Street Address: " + street address
         d. print "City: " + city
-        e. print "Zip Code: " + zip code
+        e. print "State: " + state
+        f. print "Zip Code: " + zip code
     11. End the program
 -------------------------------------------
  Program Inputs: None, all variable are hardcoded in the program
@@ -39,8 +41,21 @@ public class App {
     // }
 
     public static void main(String[] args) {
+        //assign appropriate values to each variable
+        String firstName = "John";
+        String lastName = "Battle";
+        String address = "2990 Cook Hill Road";
+        String city = "Stamford";
+        String state = "CT";
+        String zipCode = "06903";
         
+        // print name and address each on a separate line
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Address: " + address);
+        System.out.println("City: " + city);
+        System.out.println("State: " + state);
+        System.out.println("Zip Code: " + zipCode);
 
-        // System.out.println(new App().getGreeting());
     }
 }
